f0d6b7ec0dc4e6d6edaa71cee1bb7e1acd6fb981
yoshikikasama <kasamayoshiki@gmail.com>
2023-12-21 07:59:57 +0900

    20231221

--- news-app/screens/HomeScreen.js
+++ news-app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { StyleSheet, SafeAreaView, ScrollView, FlatList} from 'react-native';
+import { useEffect, useState , useRef} from 'react';
+import { StyleSheet, SafeAreaView, FlatList, RefreshControl} from 'react-native';
 import {ListItem} from "../components/ListItem";
 // import dummyArticles from "./dummies/articles";
 import axios from "axios" ;
@@ -9,11 +9,19 @@ const URL = `https://newsapi.org/v2/everything?q=tesla&from=2023-12-01&sortBy=pu
 
 export const HomeScreen = ({ navigation }) => {
   const [articles, setArticles] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const pageRef = useRef(1)
+  const fetchAllRef = useRef(false)
 
-  const fetchArticles = async() => {
+  const fetchArticles = async(page) => {
     try{
-      const response = await axios.get(URL)
-      setArticles(response.data.articles)
+      const response = await axios.get(`${URL}&page=${page}`);
+      if (response.data.articles.length > 0){
+        setArticles(prevArticles => [...prevArticles, ...response.data.articles] );
+      }else{
+        fetchAllRef.current = true;
+      }
+      
     }catch(error){
       console.error(error.response)
     }
@@ -21,7 +29,20 @@ export const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     fetchArticles()
   },[])
-
+  const onEndReached = () => {
+    if (!fetchAllRef.current){
+      pageRef.current = pageRef.current + 1;
+      fetchArticles(pageRef.current);
+    }
+  };
+  const onRefresh = async() => {
+    setRefreshing(true)
+    setArticles([])
+    pageRef.current = 1
+    fetchAllRef.current = false
+    await fetchArticles(1)
+    setRefreshing(false)
+  };
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -38,6 +59,13 @@ export const HomeScreen = ({ navigation }) => {
           )
         }}
         keyExtractor={(item, index) => index.toString()}
+        onEndReached={onEndReached}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        } 
       />
     </SafeAreaView>
   );
